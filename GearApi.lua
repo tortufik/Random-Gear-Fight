dd6f93bf7b9e6508fe394862d16761664090fb43
tortufik <135424600+tortufik@users.noreply.github.com>
2024-11-01 13:54:05 +0200

    did a thing to remove confusion

--- GearApi.lua
+++ GearApi.lua
@@ -1,3 +1,5 @@
+-- game.ReplicatedStorage.Modules.GearApi
+
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local RunService = game:GetService("RunService")
